9462777fef964376324a40a4c00ac39356490a76
SaiChandra2896 <saichandra2896@gmail.com>
2019-10-11 11:59:52 +0530

    add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>I'm SaiChandra, a full-stack developer living in Hyderabad</h2>
+    </div>
+  )
+}
+
+export default IndexPage
