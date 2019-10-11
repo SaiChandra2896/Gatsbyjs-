4e2d6a02cb410cb8000895b26cffa348b382222e
SaiChandra2896 <saichandra2896@gmail.com>
2019-10-11 12:36:52 +0530

    add header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Footer from "../components/footer"
+import Header from "../components/header"
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello!</h1>
       <h2>I'm SaiChandra, a full-stack developer living in Hyderabad</h2>
       <p>
